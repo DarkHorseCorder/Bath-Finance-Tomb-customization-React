e7eb1c51323dcbdb3b1fdc7bacc454c9815596be
DarkHorseCoder <120799088+DarkHorseCorder@users.noreply.github.com>
2023-06-02 13:05:12 +0900

    contexts

---
+++ src/contexts/Modals/Modals.tsx
@@ -0,0 +1,85 @@
+import React, { createContext, useCallback, useState } from 'react';
+import styled from 'styled-components';
+
+interface ModalsContext {
+  content?: React.ReactNode;
+  isOpen?: boolean;
+  onPresent: (content: React.ReactNode) => void;
+  onDismiss: () => void;
+}
+
+export const Context = createContext<ModalsContext>({
+  onPresent: () => {},
+  onDismiss: () => {},
+});
+
+interface StyledModalBackdropProps {
+  onClick?: () => void;
+}
+
+interface ModalProps {
+  // other props
+  onDismiss?: () => void;
+}
+
+const StyledModalBackdrop = styled.div<StyledModalBackdropProps>`
+  background-color: #00000088;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+const StyledModalWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+const Modals: React.FC = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [content, setContent] = useState<React.ReactNode>();
+
+  const handlePresent = useCallback(
+    (modalContent: React.ReactNode & { onDismiss?: () => void }) => {
+      setContent(modalContent);
+      setIsOpen(true);
+    },
+    [setContent, setIsOpen],
+  );
+
+  const handleDismiss = useCallback(() => {
+    setContent(undefined);
+    setIsOpen(false);
+  }, [setContent, setIsOpen]);
+
+  return (
+    <Context.Provider
+      value={{
+        content,
+        isOpen,
+        onPresent: handlePresent,
+        onDismiss: handleDismiss,
+      }}
+    >
+      {children}
+      {isOpen && (
+        <StyledModalWrapper>
+          <StyledModalBackdrop onClick={handleDismiss} />
+          {React.isValidElement(content) &&
+            React.cloneElement(content as React.ReactElement<ModalProps>, {
+              onDismiss: handleDismiss,
+            })}
+        </StyledModalWrapper>
+      )}
+    </Context.Provider>
+  );
+};
+
+export default Modals;
